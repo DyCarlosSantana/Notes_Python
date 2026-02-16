826c36c28986ae4b17893c31afde584d1777473c
Edy Carlos Santana <dy.carlos.santana@gmail.com>
2026-02-15 23:20:30 -0300

    Create exercicio_73.py

---
+++ Curso_em_Video/Modulo_03/Aula_16/exercicio_73.py
@@ -0,0 +1,5 @@
+numeros_extenso = ('zero', 'um', 'dois', 'três', 'quadro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'catorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
+
+num_usuario = int(input('Digite um número: '))
+
+print(f'Você digitou o número {numeros_extenso[num_usuario]}')
