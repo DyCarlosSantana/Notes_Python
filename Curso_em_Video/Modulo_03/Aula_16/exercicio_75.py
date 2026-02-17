82eeb88476f44a141d59746aef84b1a54c05b396
Edy Carlos Santana <dy.carlos.santana@gmail.com>
2026-02-16 22:36:48 -0300

    Update exercicio_75.py

--- Curso_em_Video/Modulo_03/Aula_16/exercicio_75.py
+++ Curso_em_Video/Modulo_03/Aula_16/exercicio_75.py
@@ -4,22 +4,27 @@
         b)Em que posição foi digitado o primeiro valor 3
         C) Quais foram os números pares
 '''
+'''
 while True:
     try:
         valores = []
         for p in range(1, 5): # o priemiro número marca o inicio do indice e o segunda e a quantidades de "voltas" que o loop deve dar
             valor = int(input(f'{p}° valor: '))
             valores.append(valor)
-
-        print(valores)
-        print('------------------------------------')
-        print (f'1|Quantas vezes aparece o valor 9: {valores.count(9)}')
-        print (f'2|Em que posição foi digitado o primeiro valor 3: {valores.index(3)}')
-        par = []
-        for n in valores:
-            if n % 2 == 0:
-                par.append(n)
-        print (f'3|Quais foram os números pares: {par}')
-        break
-    except:
-        print('Tente novamente!')
+'''
+            
+valores = (int(input('Digite um valor:')), 
+           int(input('Digite um valor:')),
+           int(input('Digite um valor:')),
+           int(input('Digite um valor:')))
+print(valores)
+print('------------------------------------')
+print (f'1| Quantas vezes aparece o valor 9: {valores.count(9)}')
+if 3 in valores:
+    print (f'2| Em que posição foi digitado o primeiro valor 3: {valores.index(3)+1}')
+else:
+    print('2| Em que posição foi digitado o primeiro valor 3: O valor 3 não foi digitado.')
+print (f'3| Quais foram os números pares:')
+for n in valores:
+    if n % 2 == 0:
+        print(n, end=', ')
