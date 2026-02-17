f17fc1f90d0bf2811fc498731005a94594c1c2d2
Edy Carlos Santana <dy.carlos.santana@gmail.com>
2026-02-16 22:36:42 -0300

    Update exercicio_73.py

--- Curso_em_Video/Modulo_03/Aula_16/exercicio_73.py
+++ Curso_em_Video/Modulo_03/Aula_16/exercicio_73.py
@@ -10,12 +10,15 @@ colocados_brasileirao_2025 = (
     "Botafogo", "Bahia", "São Paulo", "Grêmio", "Red Bull Bragantino", "Atlético Mineiro", "Santos", "Corinthians", "Vasco da Gama", "Vitória", "Internacional", "Ceará", "Fortaleza", "Juventude", "Sport")
 
 print(f'Os vinte colocados do brasileirão 2025: {colocados_brasileirao_2025}')
-
+print('--------------------------------------------------------------------')
 print(f'Os primeiros cinco colocados: {colocados_brasileirao_2025[0:5]}')
-print(f'Os útimos 4 colocados: {colocados_brasileirao_2025[16:20]}')
+print('--------------------------------------------------------------------')
+print(f'Os útimos 4 colocados: {colocados_brasileirao_2025[-4:]}') #motre do -4(16) para frente (até o ultimmo)
+print('--------------------------------------------------------------------')
 print(f'Lista dos times em ordem alfabetica: {sorted(colocados_brasileirao_2025)}')
+print('--------------------------------------------------------------------')
 try:
-    print(f'Posição na tabela o time Chapecoense: {colocados_brasileirao_2025.index("Chapecoense")}')
+    print(f'Posição na tabela o time Chapecoense: {colocados_brasileirao_2025.index("Chapecoense")+1}°') # +1 para se equivaler a posição na tupla
 except:
     if "Chaepcoense" not in colocados_brasileirao_2025:
         print('A posição não pode ser indentificada pois o time "Chapecoense" não esta no top 20 colocados')
