5e4c23f915571a8c771805766c91a0365fe72e9f
Edy Carlos Santana <dy.carlos.santana@gmail.com>
2026-02-18 22:02:21 -0300

    Update exercico_80.py

--- Curso_em_Video/Modulo_03/Aula_17/exercico_80.py
+++ Curso_em_Video/Modulo_03/Aula_17/exercico_80.py
@@ -3,3 +3,7 @@ Exercício 80: Lista Ordenada sem sort()
 O que fazer: Crie um programa onde o utilizador possa digitar cinco valores numéricos e registe-os numa lista, já na posição correta de inserção (sem usar o sort()). No final, mostre a lista ordenada no ecrã.
 Dica do Cleitin: Este é um exercício de lógica pura (algoritmo de ordenação manual).
 '''
+lista = []
+for i in range(1, 6):
+    num = int(input(f'Digite o {i}° número: '))
+    lista.append(num)
