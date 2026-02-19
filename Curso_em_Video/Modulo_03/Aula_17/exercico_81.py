64a276748ce69c8f3a4871b2c77a414de6260e8b
Edy Carlos Santana <dy.carlos.santana@gmail.com>
2026-02-18 22:02:24 -0300

    Update exercico_81.py

--- Curso_em_Video/Modulo_03/Aula_17/exercico_81.py
+++ Curso_em_Video/Modulo_03/Aula_17/exercico_81.py
@@ -5,3 +5,24 @@ O que fazer: Crie um programa que vai ler vários números e colocar numa lista.
     2. A lista de valores, ordenada de forma decrescente.
     3. Se o valor 5 foi digitado e está ou não na lista.
 '''
+lista = []
+while True:
+    try:
+        lista.append(int(input('Digite um número: ')))
+        sn = input('Deseja continuar: [S/N]-> ').upper()
+        if sn == 'N': 
+            break
+        elif sn == 'S':
+            continue
+    except:
+        print(f'Tente Novamente!', end=' ')
+
+print(f'Foram digitados {len(lista)} números;')
+
+lista.sort(reverse=True)
+print(f'Os números digitados forma: {lista}')
+
+if 5 in lista:
+    print('O número 5 faz parte da lista')
+else:
+    print('O número  não foi digitado')
