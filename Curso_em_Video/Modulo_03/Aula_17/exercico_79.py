5dd707e6e2ac872926c639c191c9cf0d8e8fe6c9
Edy Carlos Santana <dy.carlos.santana@gmail.com>
2026-02-18 22:02:18 -0300

    Update exercico_79.py

--- Curso_em_Video/Modulo_03/Aula_17/exercico_79.py
+++ Curso_em_Video/Modulo_03/Aula_17/exercico_79.py
@@ -2,3 +2,28 @@
 Exercício 79: Valores Únicos numa Lista
 O que fazer: Crie um programa onde o utilizador possa digitar vários valores numéricos e registe-os numa lista. Caso o número já exista lá dentro, ele não deve ser adicionado. No final, serão exibidos todos os valores únicos digitados, em ordem crescente.
 '''
+
+valores = []
+
+while True:
+
+    try:
+        novo_valor = (int(input('Digite um número: ')))
+
+        if novo_valor not in valores:
+            valores.append(novo_valor)
+
+        sn = input('Deseja continuar [S/N]-> ').upper()
+        if sn == 'N':
+            break
+        elif sn == 'S':
+            continue
+
+    except:
+        print('Tente novamente!', end=' ')
+
+valores.sort()
+print(f'Os números digitados foram: {valores}')
+
+    
+    
