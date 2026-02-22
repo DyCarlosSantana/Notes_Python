ba52c437c3dacdff916f6c685985ba93a61201cf
Edy Carlos Santana <dy.carlos.santana@gmail.com>
2026-02-21 23:42:03 -0300

    Create geraçao_automatica.py
    Script para geração automatica de arquivos em python para resolução dos exercicios

---
+++ Curso_em_Video/Modulo_03/geraçao_automatica.py
@@ -0,0 +1,20 @@
+import os
+
+# Defininfo o caminho
+diretorio = 'Aula_18'
+# Cria o diretorio/pasta caso não exista
+if not os.path.exists(diretorio):
+    os.makedirs(diretorio)
+    print("Novo diretorio criado")
+
+# Define a quantidade de arquivos e o prefixo
+quantidade = 89
+prefixo = "exercicio"
+
+for i in range(84, quantidade + 1):
+    nome_arquivo = f"{prefixo}_{i}.py"
+    caminho = os.path.join(diretorio, nome_arquivo)
+    with open(caminho, "w", encoding="utf-8") as f:
+        f.write(f"# Este é o arquivo número {i}\n")
+    print(f"Criado: {nome_arquivo} em {diretorio}")
+
