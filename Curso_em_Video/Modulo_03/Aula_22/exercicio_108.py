f6eb13c599c7bac9e3108f0724a848f73eb3fa4e
Edy Carlos Santana <dy.carlos.santana@gmail.com>
2026-06-08 22:52:01 -0300

    update aula 22
    Criação de pacotes, organização de sub-pacotes

--- Curso_em_Video/Modulo_03/Aula_22/exercicio_108.py
+++
@@ -1,5 +0,0 @@
-# Este é o arquivo número 108
-"""
-Exercício 108: Formatando Moedas em Python
-O que fazer: Adapte o código do desafio 107, criando uma função adicional chamada moeda() que consiga mostrar os números como um valor monetário formatado (ex: R$10,00).
-"""
--- Curso_em_Video/Modulo_03/Aula_22/main.py
+++ Curso_em_Video/Modulo_03/Aula_22/main.py
@@ -1,5 +1,9 @@
-# -- Programa principal --
-import moeda
+# Importando os submódulos dos pacotes de utilidades
+from utilidadesCeV.moeda import moeda
+from utilidadesCeV.dado import dado
 
-num = float(input("Digite um valor: "))
+# Usando a função leiaDinheiro (Ex 112) criada para garantir dados limpos
+num = dado.leiaDinheiro("Digite um preço: R$")
+
+# Exibimos o resumo (Ex 110) configurado com 20% de aumento e 20% de redução
 moeda.resumo(num, 20, 20)
