fac0ec8302940875f4245b440f3886d941c41699
Edy Carlos Santana <dy.carlos.santana@gmail.com>
2026-06-19 18:58:55 -0300

    update desafios
    diretorio desafios + arquivos .py
    enunciado dos desafios 16 a 22, e gerador automatico de arquivos.

---
+++ Curso_em_Video/Modulo_04/desafios/desafio_16.py
@@ -0,0 +1,7 @@
+# Desafio 16
+"""
+    Desafio 16: Classe Funcionário (Introdução)
+    O que fazer: Crie uma classe chamada Funcionario que represente um colaborador de uma empresa.
+    O construtor deve inicializar os atributos de instância: nome, setor e cargo.
+    Instancie pelo menos dois funcionários diferentes e mostre os seus dados na tela.
+"""
---
+++ Curso_em_Video/Modulo_04/desafios/desafio_22.py
@@ -0,0 +1,8 @@
+# Desafio 22
+"""
+    Desafio 22: O Simulador de Controle Remoto
+    O que fazer: Crie uma classe chamada ControleRemoto que gerencie os comandos de uma televisão.
+    Defina atributos para controlar o canal_atual, o volume_atual e o limite de volume (por exemplo, volume máximo = 5).
+    Crie métodos comportamentais para subir e descer o volume (respeitando os limites mínimos e máximos) e mudar de canal (para cima ou para baixo).
+    Implemente um método de controle principal que receba comandos do usuário (como símbolos ou números) no terminal e acione os métodos correspondentes da televisão.
+"""
