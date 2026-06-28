ae9077e22169c5d2ea95119fe72d6792f1c21b8d
Edy Carlos Santana <dy.carlos.santana@gmail.com>
2026-06-28 13:21:48 -0300

    update exercicios extras

--- Curso_em_Video/Modulo_04/exercicios_extras/AvaliaçãoFisica/classesAvaliacao.py
+++ Curso_em_Video/Modulo_04/exercicios_extras/AvaliaçãoFisica/classesAvaliacao.py
@@ -18,13 +18,14 @@ class AvaliacaoFisica(ABC):
     def gerar_resultado(self):
         pass
 
+
 class AvaliacaoIMC(AvaliacaoFisica):
     def __init__(self, data_avaliacao, nome, idade, peso, altura):
         super().__init__(data_avaliacao, nome, idade, peso, altura)
 
     def gerar_resultado(self):
         """
-        Calcula o Índice de Massa Corporal (IMC) e retorna a classificação correspondente.
+        Calcula o Índice de Massa Corporal (IMC) para ADULTOS (19 a 59 anos), e retorna a classificação correspondente.
         Abaixo do peso: Abaixo de 18,5
         Peso normal: Entre 18,5 e 24,9
         Sobrepeso: Entre 25 e 29,9
@@ -35,18 +36,41 @@ class AvaliacaoIMC(AvaliacaoFisica):
         imc = self.peso / (self.altura ** 2)
         if imc < 18.5:
             classificacao = "Abaixo do peso"
-        elif 18.5 <= imc < 24.9:
+        elif imc < 25:
             classificacao = "Peso normal"
-        elif 25 <= imc < 29.9:
+        elif imc < 30:
             classificacao = "Sobrepeso"
-        elif 30 <= imc < 34.9:
+        elif imc < 35:
             classificacao = "Obesidade Grau I"
-        elif 35 <= imc < 39.9:
+        elif imc < 40:
             classificacao = "Obesidade Grau II"
         else:
             classificacao = "Obesidade Grau III (Mórbida)"
 
         return f"IMC: {imc:.2f} - Classificação: {classificacao}"
+
+
+class AvaliacaoIMCIdoso(AvaliacaoFisica):
+    def __init__(self, data_avaliacao, nome, idade, peso, altura):
+        super().__init__(data_avaliacao, nome, idade, peso, altura)
+
+    def gerar_resultado(self):
+        """
+        Calcula o Índice de Massa Corporal (IMC) para "IDOSOS" (a partir dos 60 anos), e retorna a classificação correspondente.
+        Abaixo de 22: Baixo peso
+        Entre 22 e 27: Peso adequado
+        Acima de 27: Sobrepeso / Obesidade .
+        """
+        imc = self.peso / (self.altura ** 2)
+        if imc < 22:
+            classificacao = "Abaixo do peso"
+        elif imc < 27:
+            classificacao = "Peso normal"
+        else:
+            classificacao = "Sobrepeso / Obesidade"
+
+        return f"IMC: {imc:.2f} - Classificação: {classificacao}" 
+
 
 class AvaliacaoMassaMagra(AvaliacaoFisica):
     def __init__(self, data_avaliacao, nome, idade, peso, altura, percentual_gordura):
