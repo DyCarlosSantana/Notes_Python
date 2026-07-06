0007e44d3d13c035055aa171887d8ab1db434edd
Edy Carlos Santana <dy.carlos.santana@gmail.com>
2026-07-06 12:27:48 -0300

    Update desafio_26.py

--- Curso_em_Video/Modulo_04/desafios/desafio_26/desafio_26.py
+++ Curso_em_Video/Modulo_04/desafios/desafio_26/desafio_26.py
@@ -1,1 +1,17 @@
 # Desafio 26
+from abc import ABC, abstractmethod
+
+class Funcionario(ABC):
+    def __init__(self, nome, salario_bruto, salario_liquido):
+        self.nome = nome
+        self.salario_bruto = salario_bruto
+        self.salario_liquido = salario_liquido
+        self.salario_min = 1621.00
+        self.inss - 7.5
+
+    @abstractmethod
+    def calcular_salario_liquido(self):
+        pass
+
+    def analisar_salario(self):
+        pass # função concreta
