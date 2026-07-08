03ed47b3d8d38e3dcc3bc6cd9d17a3542cc02436
Edy Carlos Santana <dy.carlos.santana@gmail.com>
2026-07-08 10:15:06 -0300

    Update __main__.py

--- Curso_em_Video/Modulo_04/desafios/desafio_26/__main__.py
+++ Curso_em_Video/Modulo_04/desafios/desafio_26/__main__.py
@@ -2,9 +2,13 @@ from desafio_26 import *
 
 
 def main():
-    f1 = FuncionarioHorista("Claudio", 12, 200)
+    f1 = FuncionarioHorista("Claudio")
     f1.calcular_salario_liquido()
     f1.analisar_salario()
+
+    f2 = FuncionarioMensalista("Karolina", 6000)
+    f2.calcular_salario_liquido()
+    f2.analisar_salario()
 
 if __name__ == "__main__":
     main()
