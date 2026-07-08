7514486c28e39f65bfa4cde50e02998c92390d7c
Edy Carlos Santana <dy.carlos.santana@gmail.com>
2026-07-08 10:15:14 -0300

    Update desafio_26.py

--- Curso_em_Video/Modulo_04/desafios/desafio_26/desafio_26.py
+++ Curso_em_Video/Modulo_04/desafios/desafio_26/desafio_26.py
@@ -6,9 +6,9 @@ from rich.console import Console
 
 class Funcionario(ABC):
     console = Console()
-    def __init__(self, nome, salario_bruto):
+    def __init__(self, nome):
         self.nome = nome
-        self.salario_bruto = salario_bruto
+        self.salario_bruto = 0
         self.salario_liquido = 0
         self.salario_min = 1621.00
         self.inss = 7.5
@@ -17,29 +17,31 @@ class Funcionario(ABC):
     def calcular_salario_liquido(self):
         pass
 
+
     def analisar_salario(self):
-        conteudo = f"O salario de [blue]{self.nome}[/] é de [green]R${self.salario_liquido}[/] e corresponde a [yellow](...) salarios mínimos[/]."
-        painel = Panel(conteudo, title="Análise de Sálario")
+        base = self.salario_liquido / self.salario_min
+        conteudo = f"O salario de [blue]{self.nome}[/] ({self.__class__.__name__}) é de [green]R${self.salario_liquido:.2f}[/] e corresponde a [yellow]{base:.1f} salarios mínimos[/]."
+        painel = Panel.fit(conteudo, title="Análise de Sálario", width=60)
 
         Funcionario.console.print(painel)
 
 
 class FuncionarioHorista(Funcionario):
-    def __init__(self, nome, valor_horas, qtd_horas):
-        super().__init__(nome, salario_bruto = 0)
+    def __init__(self, nome, valor_horas = 7.37, qtd_horas = 220):
+        super().__init__(nome)
         self.valor_horas = valor_horas
         self.qtd_horas = qtd_horas
+        self.salario_bruto = self.valor_horas * self.qtd_horas
     
     def calcular_salario_liquido(self):
-        self.salario_bruto = self.valor_horas * self.qtd_horas
-        valor_desconto = (self.salario_bruto * self.inss) / 100
-        self.salario_liquido = self.salario_bruto - valor_desconto
+        self.salario_liquido = self.salario_bruto - ((self.salario_bruto * self.inss) / 100)
         return self.salario_liquido
 
 
 class FuncionarioMensalista(Funcionario):
     def __init__(self, nome, salario_bruto):
-        super().__init__(nome, salario_bruto)
+        super().__init__(nome)
+        self.salario_bruto = salario_bruto
 
     def calcular_salario_liquido(self):
         valor_desconto = (self.salario_bruto * self.inss) / 100
